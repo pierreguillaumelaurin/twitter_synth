dba798419f3aa2d7a4671d8691aab15b6ad9ff3e
Pierre-Guillaume <pierreguillaumelaurin@gmail.com>
2016-05-31 19:51:25 -0400

    Add Tweetie-master file

---
+++ app/Tweetie-master/api/config.php
@@ -0,0 +1,16 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+    // Consumer Key
+    define('CONSUMER_KEY', 'mJ5otbhPGNoDCgCL7j5g');
+    define('CONSUMER_SECRET', 'M0XkT6GeBnjxdlWHcSGYX1JutMVS9D5ISlkqRfShg');
+
+    // User Access Token
+    define('ACCESS_TOKEN', '4878723639-UacBTUlpPUB2MOVexsjpLiOluLprXXRa0v1Dmg2');
+    define('ACCESS_SECRET', 'cp2alhKu904LfEdrvmgY7Ag2YQJC3mEwSRQwJ9FIQ4mzt');
+	
+	// Cache Settings
+	define('CACHE_ENABLED', false);
+	define('CACHE_LIFETIME', 3600); // in seconds
+	define('HASH_SALT', md5(dirname(__FILE__)));
